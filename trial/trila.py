901e952ba9fc5edda316e4ec159b2c22f833ec00
omar-alsawaf <99086491+omar-alsawaf@users.noreply.github.com>
2022-05-06 16:48:11 +0200

    trial 3la al main structure

---
+++ trial/trila.py
@@ -0,0 +1,13 @@
+def ctd5():
+    import ctd5
+
+def ctd4():
+    import ctd4
+
+from tkinter import *
+root=Tk()
+root.title("misssion 3.2")
+root.geometry('1000x1000')
+Button(root, text ="ctd3", command= ctd5 , height=3 ,width=10).place(x=50, y=150)
+Button(root, text ="ctd4", command= ctd4 , height=3 ,width=10).place(x=150, y=150)
+root.mainloop()
